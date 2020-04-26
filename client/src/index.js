5f4be4eff3800abaeceb78f492b36c26570bf284
leosul@gmail.com <leosul@gmail.com>
2020-04-26 17:45:41 +0100

    Created Signin.css

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import routes from './routes'
+require('dotenv').config()
 
-ReactDOM.render( routes,
+ReactDOM.render(routes,
     document.getElementById('root')
 );
