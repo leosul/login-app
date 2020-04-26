af16387586f20888229261e77fa204d208b9c019
leosul@gmail.com <leosul@gmail.com>
2020-04-26 17:28:08 +0100

    Fix Signin component

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-import MainContainer from './pages/main/MainContainer'
+//import MainContainer from './pages/main/MainContainer'
+import SigninContainer from './pages/signin/SigninContainer'
 
-ReactDOM.render(
-    <MainContainer />,
-  document.getElementById('root')
+ReactDOM.render( <
+    SigninContainer / > ,
+    document.getElementById('root')
 );
