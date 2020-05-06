ef43cacc0d983801508c58f3520ab92b494746d0
leosul@gmail.com <leosul@gmail.com>
2020-05-06 22:57:24 +0100

    Fix commands

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('./server/app')
 const { connect } = require('./server/database/connection')
 const port = process.env.PORT
 
-app.listen(port || 3000, async() => {
+app.listen(port, async() => {
     console.log(`API - Listening on port ${port}`)
     await connect()
     console.log(`Connected to MongoDB`)
