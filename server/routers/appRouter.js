c9372757b4360ed0bc148d67475d49ef1e971837
leosul@gmail.com <leosul@gmail.com>
2020-05-06 21:36:16 +0100

    Routes adjusted to respect authentication

--- server/routers/appRouter.js
+++ server/routers/appRouter.js
@@ -1,19 +1,17 @@
-const { Router } = require('express')
-const authenticator = require('./../middlewares/authenticator')
-const customerRouter = require('./customerRouter')
-const countryRouter = require('./countryRouter')
-const summaryRouter = require('./summaryRouter')
-const authRouter = require('./authRouter')
-const router = new Router()
+const { Router } = require("express");
+const authenticator = require("./../middlewares/authenticator");
+const countryRouter = require("./countryRouter");
+const summaryRouter = require("./summaryRouter");
+const authRouter = require("./authRouter");
+const router = new Router();
 
-router.get('/', (_req, res) => {
-    res.send('LoginApp API')
-})
+router.get("/", (_req, res) => {
+    res.send("LoginApp API");
+});
 
-router.use('/customers', authenticator, customerRouter)
-router.use('/auth', authRouter)
-router.use('/countries', countryRouter)
-router.use('/countries/:country', countryRouter)
-router.use('/summary', summaryRouter)
+router.use("/auth", authRouter);
+router.use("/countries", authenticator, countryRouter);
+router.use("/countries/:country", authenticator, countryRouter);
+router.use("/summary", authenticator, summaryRouter);
 
-module.exports = router
+module.exports = router;
